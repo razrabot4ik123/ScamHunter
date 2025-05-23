a1e6779b1ea843e29af1e7b6de680a81c0ebbf57
razrabot4ik123 <IIIRaZRaBoT4IKIII@yandex.ru>
2025-05-23 23:39:08 +0700

    Create Project

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',
+  base: '/scamhunter/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  publicDir: './src/assets/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
+    },
+  },
+});
