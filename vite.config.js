2ec8a95f631b1745e9ea24e7c372d63b66c5bbd4
razrabot4ik123 <IIIRaZRaBoT4IKIII@yandex.ru>
2025-05-28 21:47:56 +0700

    change base directory

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: './src',
-  base: '/scamhunter/',
+  base: '/ScamHunter/',
   server: {
     port: 3000,
     open: true,
